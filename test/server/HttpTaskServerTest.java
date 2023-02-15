d788b6f4ca172d64c78c0586613427fe7c4e5fcc
Max <dusa11223344@yandex.ru>
2023-02-15 22:33:18 +0300

    Идет работа

--- test/server/HttpTaskServerTest.java
+++ test/server/HttpTaskServerTest.java
@@ -3,10 +3,12 @@ package server;
 import adapter.LocalDateTimeAdapter;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import manager.Managers;
 import manager.file.FileBackedTasksManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import tasks.Epic;
 import tasks.SubTask;
 import tasks.Task;
@@ -14,9 +16,16 @@ import tasks.enums.StatusTypeEnum;
 import tasks.enums.Type;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class HttpTaskServerTest {
     HttpTaskServer server;
@@ -50,5 +59,30 @@ class HttpTaskServerTest {
         server.serverStop();
     }
 
+    @Test
+    void get() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        URI url = URI.create("http://localhost:8080/tasks/task");
+        HttpRequest request = HttpRequest.newBuilder().uri(url).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(200, response.statusCode());
+        java.lang.reflect.Type tasksType = new TypeToken<ArrayList<Task>>() {
+        }.getType();
+        List<Task> list = gson.fromJson(response.body(), tasksType);
+        assertNotNull(list);
+    }
 
+    @Test
+    void getById() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        URI url = URI.create("http://localhost:8080/tasks/task?id=1");
+        HttpRequest request = HttpRequest.newBuilder().uri(url).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        assertEquals(200, response.statusCode());
+        java.lang.reflect.Type tasksType = new TypeToken<ArrayList<Task>>() {
+        }.getType();
+        List<Task> list = gson.fromJson(response.body(), tasksType);
+        assertNotNull(list);
+        assertEquals(task, list.get(0));
+    }
 }
